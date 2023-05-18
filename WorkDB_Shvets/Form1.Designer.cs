fdbc3c80faf2c2d897088ccfd8a2d014f65ed39e
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-05-18 11:47:39 +0500

    Add files via upload

---
+++ WorkDB_Shvets/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WorkDB_Shvets
+{
+    partial class Authorization_frm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.securityDB_ShvetsDataSet = new WorkDB_Shvets.SecurityDB_ShvetsDataSet();
+            this.securityDBShvetsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usertblBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.user_tblTableAdapter = new WorkDB_Shvets.SecurityDB_ShvetsDataSetTableAdapters.User_tblTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loginDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.activeDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.roleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDB_ShvetsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDBShvetsDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usertblBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(96, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(61, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox.Location = new System.Drawing.Point(159, 110);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 29);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginTextBox.Location = new System.Drawing.Point(159, 63);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(100, 29);
+            this.loginTextBox.TabIndex = 3;
+            // 
+            // inputButton
+            // 
+            this.inputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputButton.Location = new System.Drawing.Point(127, 176);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(109, 64);
+            this.inputButton.TabIndex = 4;
+            this.inputButton.Text = "Login in application";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.loginDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn,
+            this.countDataGridViewTextBoxColumn,
+            this.dateDataGridViewTextBoxColumn,
+            this.activeDataGridViewCheckBoxColumn,
+            this.roleDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.usertblBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 369);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(748, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // securityDB_ShvetsDataSet
+            // 
+            this.securityDB_ShvetsDataSet.DataSetName = "SecurityDB_ShvetsDataSet";
+            this.securityDB_ShvetsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // securityDBShvetsDataSetBindingSource
+            // 
+            this.securityDBShvetsDataSetBindingSource.DataSource = this.securityDB_ShvetsDataSet;
+            this.securityDBShvetsDataSetBindingSource.Position = 0;
+            // 
+            // usertblBindingSource
+            // 
+            this.usertblBindingSource.DataMember = "User_tbl";
+            this.usertblBindingSource.DataSource = this.securityDB_ShvetsDataSet;
+            // 
+            // user_tblTableAdapter
+            // 
+            this.user_tblTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // loginDataGridViewTextBoxColumn
+            // 
+            this.loginDataGridViewTextBoxColumn.DataPropertyName = "Login";
+            this.loginDataGridViewTextBoxColumn.HeaderText = "Login";
+            this.loginDataGridViewTextBoxColumn.Name = "loginDataGridViewTextBoxColumn";
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            // 
+            // countDataGridViewTextBoxColumn
+            // 
+            this.countDataGridViewTextBoxColumn.DataPropertyName = "Count";
+            this.countDataGridViewTextBoxColumn.HeaderText = "Count";
+            this.countDataGridViewTextBoxColumn.Name = "countDataGridViewTextBoxColumn";
+            // 
+            // dateDataGridViewTextBoxColumn
+            // 
+            this.dateDataGridViewTextBoxColumn.DataPropertyName = "Date";
+            this.dateDataGridViewTextBoxColumn.HeaderText = "Date";
+            this.dateDataGridViewTextBoxColumn.Name = "dateDataGridViewTextBoxColumn";
+            // 
+            // activeDataGridViewCheckBoxColumn
+            // 
+            this.activeDataGridViewCheckBoxColumn.DataPropertyName = "Active";
+            this.activeDataGridViewCheckBoxColumn.HeaderText = "Active";
+            this.activeDataGridViewCheckBoxColumn.Name = "activeDataGridViewCheckBoxColumn";
+            // 
+            // roleDataGridViewTextBoxColumn
+            // 
+            this.roleDataGridViewTextBoxColumn.DataPropertyName = "Role";
+            this.roleDataGridViewTextBoxColumn.HeaderText = "Role";
+            this.roleDataGridViewTextBoxColumn.Name = "roleDataGridViewTextBoxColumn";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(127, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 64);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Register in application";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Authorization_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 576);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.inputButton);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Authorization_frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Для входа введите логин и пароль";
+            this.Load += new System.EventHandler(this.Authorization_frm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDB_ShvetsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDBShvetsDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usertblBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource securityDBShvetsDataSetBindingSource;
+        private SecurityDB_ShvetsDataSet securityDB_ShvetsDataSet;
+        private System.Windows.Forms.BindingSource usertblBindingSource;
+        private SecurityDB_ShvetsDataSetTableAdapters.User_tblTableAdapter user_tblTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loginDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn activeDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WorkDB_Shvets/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkDB_Shvets
+{
+    public partial class Authorization_frm : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SecurityDB_Shvets;Integrated Security=True");
+        public Authorization_frm()
+        {
+            InitializeComponent();
+            reloadTable();
+        }
+       
+        private void Authorization_frm_Load(object sender, EventArgs e)
+        {}
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            if (userExist(1, loginTextBox.Text) && userExist(2,passwordTextBox.Text))
+            {
+                MessageBox.Show("Вы успешно вошли");
+            }
+        }
+        private void reloadTable()
+        {
+            this.user_tblTableAdapter.Fill(this.securityDB_ShvetsDataSet.User_tbl);
+        }
+        private bool check()
+        {
+            if (loginTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Поле логин пустое");
+                return false;
+            }
+            if (passwordTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Поле пароль пустое");
+                return false;
+            }
+            return true;
+        }
+        private bool userExist(int column,string field)
+        {
+            for (int i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                if (dataGridView1[column, i].Value.ToString() == field)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void addUser(string login,string password,int count,string date,bool active,string role)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand($"INSERT INTO dbo.User_tbl(login, password, count, date, active, role)VALUES('{login}', '{password}', 0, '2023-05-1', 'True', 'user')",connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            reloadTable();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (check())
+            {
+                if (!userExist(1, loginTextBox.Text))
+                {
+                    addUser(loginTextBox.Text, passwordTextBox.Text, 0, "2023-05-1", true, "user");
+                }
+            }
+        }
+    }
+}
